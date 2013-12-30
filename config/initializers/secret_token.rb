4dcd06aff3e9ee38b687541e65996e45a40bd345
Oleg <soccerboot13@live.com>
2013-12-30 11:07:00 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NukeClan::Application.config.secret_key_base = 'f80c64721f1ad2862e7070dcad561629242c40a8d2f111d784f8b30d24dccc7f2ddc6b57d4db0efdb66f8839dea055350b9dbc44e8fcaeed535f5027052fa716'
